406a8c544ee98872c4f037e5c052ffbda448751d
Kyungwon Kim <141529620+y5ask82@users.noreply.github.com>
2023-08-24 10:51:18 +0900

    test1
    test1

---
+++ ConsoleApp21/ConsoleApp21/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        public string Solution(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd";
+            }
+        }
+
+        private static string solution(int num)
+        {
+            string answer = "";
+            return answer;
+        }
+
+        static void Main(string[] args)
+
+        {
+            Console.Write("번호를 입력하세요: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("짝수입니다.");
+            }
+            else
+            {
+                Console.WriteLine("홀수입니다.");
+            }
+
+        }
+    }
+        
+}
+
+    
+
+
+  
