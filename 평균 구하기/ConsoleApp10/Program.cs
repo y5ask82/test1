c61505431a2452be429fa3492e4b6f690b05197f
Kyungwon Kim <141529620+y5ask82@users.noreply.github.com>
2023-08-25 10:58:08 +0900

    Add files via upload

---
+++ 평균 구하기/ConsoleApp10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    public class Solution
+    {
+        public double solution(int[] arr)
+        {
+            int sum = 0;
+            foreach (int num in arr)
+            {
+                sum += num;
+            }
+            return (double)sum / arr.Length;
+        }
+    }
+}
